5c98851fa9089a837dde5e7e71e9a602de757479
luanheider <luanheider@hotmail.com>
2025-02-27 13:53:15 -0400

    inicial commit

---
+++ src/main/java/com/luanheider/apiusuario/ApiUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.luanheider.apiusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsuarioApplication.class, args);
+	}
+
+}
